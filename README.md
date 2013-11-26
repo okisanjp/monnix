043ab4f147e3140a9f7409b1750009d1ab8eff79
okisanjp <okisan.jp@gmail.com>
2013-11-26 14:26:14 +0900

    画面リロードをjavascriptに変更＆残り時間バーの実装

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 monnix
 ======
 
-PhpZabbixApiを利用しています
- * http://zabbixapi.confirm.ch/
-
+## コレは何？
 Zabbixの障害数をシンプルに表示
  * zabbix server 2.0.9を相手に開発
  * 現場で使うために作った簡単なツールです
  * 時間を見て改良していきますが動作は無保証
 
+## 利用ライブラリ・コード参考元
+ * [twitter bootstrap progress barプログレスバー ページ読込アニメーション(jQuery)///More bootstrap](http://weblibrary.s224.xrea.com/bootstrap/snippets/twitter_bootstra_1.html)
+ ** 上記コードに若干の修正（bootstrap3対応、長さの加算を減算に変更、ゼロになったらページリロードを加筆）しました。
+ * [PhpZabbixApi](http://zabbixapi.confirm.ch/)
--- webroot/index.php
+++ webroot/index.php
@@ -9,14 +9,38 @@ require '../class/ZabbixApi.class.php';
 <meta charset="utf-8">
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="Refresh" content="30">
 <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css"
 	href="./css/bootstrap-theme.min.css" />
 <link rel="stylesheet" type="text/css" href="./css/style.css" />
+<script src="./js/jquery-1.10.2.min.js" type="text/javascript"
+	charset="utf-8"></script>
 <script src="./js/bootstrap.min.js" type="text/javascript"
 	charset="utf-8"></script>
 <title>monnix</title>
+<script>
+var run = function(time) {
+	return $.Deferred(function(dfd) {
+	setTimeout(dfd.resolve, time)
+	}).promise();
+}
+run(300).then(function() {
+	$('.progress .progress-bar').each(function() {
+		var $this = $(this)
+		, rate = $this.attr('data-rate')
+		, current = 100;
+		var progress = setInterval(function() {
+			if(current <= rate) {
+				clearInterval(progress);
+				location.reload();
+			} else {
+				current -= 1;
+				$this.css('width', (current)+ '%');
+			}
+		}, 250);
+	});
+});
+</script>
 </head>
 <div class="container">
 	<h1>
@@ -35,12 +59,12 @@ try {
 			'output' => array (
 					"triggerid",
 					"description",
-					"priority"
+					"priority" 
 			),
 			'filter' => array (
 					'status' => 0,
-					'value' => 1
-			)
+					'value' => 1 
+			) 
 	) );
 	$count_1 = 0;
 	$count_2 = 0;
@@ -78,45 +102,60 @@ try {
 				break;
 			default :
 		}
-		$desc .= '<tr><td><span class="label label-' . $facility . '">' . $level . '</span></td><td><strong>'.$t->hostname.'</strong></td><td>' . $t->description . '</td></tr>';
+		$desc .= '<tr><td><span class="label label-' . $facility . '">' . $level . '</span></td><td><strong>' . $t->hostname . '</strong></td><td>' . $t->description . '</td></tr>';
 	}
-	//var_dump ( $trigger );
+	// var_dump ( $trigger );
 	$desc .= '</table>';
 } catch ( Exception $e ) {
 	// Exception in ZabbixApi catched
 	echo $e->getMessage ();
 }
 ?>
-
-	<div class="col-md-3">
-		<h3>
-			<span class="label label-danger">DISA</span>
-		</h3>
-		<p class="count_danger<?php if($count_5 < 1){ echo " count_zero";}?>"><?php echo $count_5;?></p>
+	<div class="col-md-6">
+		<div class="row">
+			<div class="col-md-6">
+				<h3>
+					<span class="label label-danger">DISA</span>
+				</h3>
+				<p
+					class="count_danger<?php if($count_5 < 1){ echo " count_zero";}?>"><?php echo $count_5;?></p>
+			</div>
+			<div class="col-md-6">
+				<h3>
+					<span class="label label-danger">HIGH</span>
+				</h3>
+				<p
+					class="count_danger<?php if($count_4 < 1){ echo " count_zero";}?>"><?php echo $count_4;?></p>
+			</div>
+		</div>
 	</div>
-	<div class="col-md-3">
-		<h3>
-			<span class="label label-danger">HIGH</span>
-		</h3>
-		<p class="count_danger<?php if($count_4 < 1){ echo " count_zero";}?>"><?php echo $count_4;?></p>
-	</div>
-	<div class="col-md-2">
-		<h3>
-			<span class="label label-warning">AVER</span>
-		</h3>
-		<p class="count_warning<?php if($count_3 < 1){ echo " count_zero";}?>"><?php echo $count_3;?></p>
-	</div>
-	<div class="col-md-2">
-		<h3>
-			<span class="label label-warning">WARN</span>
-		</h3>
-		<p class="count_warning<?php if($count_2 < 1){ echo " count_zero";}?>"><?php echo $count_2;?></p>
-	</div>
-	<div class="col-md-2">
-		<h3>
-			<span class="label label-info">INFO</span>
-		</h3>
-		<p class="count_info<?php if($count_1 < 1){ echo " count_zero";}?>"><?php echo $count_1;?></p>
+	<div class="col-md-6">
+		<div class="row">
+			<div class="col-md-4">
+				<h3>
+					<span class="label label-warning">AVER</span>
+				</h3>
+				<p
+					class="count_warning<?php if($count_3 < 1){ echo " count_zero";}?>"><?php echo $count_3;?></p>
+			</div>
+			<div class="col-md-4">
+				<h3>
+					<span class="label label-warning">WARN</span>
+				</h3>
+				<p
+					class="count_warning<?php if($count_2 < 1){ echo " count_zero";}?>"><?php echo $count_2;?></p>
+			</div>
+			<div class="col-md-4">
+				<h3>
+					<span class="label label-info">INFO</span>
+				</h3>
+				<p class="count_info<?php if($count_1 < 1){ echo " count_zero";}?>"><?php echo $count_1;?></p>
+			</div>
+		</div>
+		<div class="progress progress-striped active">
+			<div class="progress-bar progress-bar-success" role="progressbar"
+				style="width: 100%" data-rate="0"></div>
+		</div>
 	</div>
 	<hr />
 	<div class="row">
